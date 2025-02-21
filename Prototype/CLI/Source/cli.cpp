681557df0dea9e7f7a884573d5e6792b56473a62
Jaime Rios <3794310+AhiyaHiya@users.noreply.github.com>
2025-02-21 10:49:46 -0700

    Add Prototype CLI project

---
+++ Prototype/CLI/Source/cli.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main(int argc, char* argv[])
+{
+    if (argc > 1)
+    {
+        std::cout << "Arguments:" << std::endl;
+        for (int i = 1; i < argc; ++i)
+        {
+            std::cout << argv[i] << std::endl;
+        }
+    } else {
+        std::cout << "No arguments provided." << std::endl;
+    }
+    return 0;
+}
