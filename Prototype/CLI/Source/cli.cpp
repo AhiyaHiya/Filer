351e51b7570a85f02d62227ff0ab92af6bf210f1
Jaime Rios <3794310+AhiyaHiya@users.noreply.github.com>
2025-05-15 23:06:12 -0700

    Added start of spdlog usage

--- Prototype/CLI/Source/cli.cpp
+++ Prototype/CLI/Source/cli.cpp
@@ -1,5 +1,9 @@
 
+#include "last_file_path.hpp"
+
 #include <ftxui/component/screen_interactive.hpp>
+#include <spdlog/async.h>
+#include <spdlog/sinks/stdout_color_sinks.h>
 
 #include <iostream>
 #include <string>
@@ -9,19 +13,12 @@ constexpr auto errorDetected = 1;
 
 int main(int argc, char *argv[])
 {
-    // Get the home directory from the environment variable "HOME"
-    const auto homeEnv = std::getenv("HOME");
-    if (homeEnv == nullptr)
-    {
-        std::cerr << "Error: HOME environment variable not set.\n";
-        return errorDetected;
-    }
-    const auto home = std::string(homeEnv);
-    if (home.empty())
-    {
-        std::cerr << "Error: HOME variable contains empty path.\n";
-        return errorDetected;
-    }
+    spdlog::init_thread_pool(8192, 1); // Queue size and thread count
+    auto async_logger = spdlog::create_async<spdlog::sinks::stdout_color_sink_mt>("async_console");
+    async_logger->info("Async logging enabled!");
+
+    // We are going to use the last know path to open the app to that folder
+    const auto lastPath = get_last_path();
 
     auto screen = ftxui::ScreenInteractive::TerminalOutput();
 
--- Prototype/CLI/Source/last_file_path.cpp
+++ Prototype/CLI/Source/last_file_path.cpp
@@ -1,7 +1,25 @@
 
 #include "last_file_path.hpp"
 
+#include <spdlog/async.h>
+#include <spdlog/sinks/stdout_color_sinks.h>
+
+#include <iostream>
+
 auto get_last_path() -> std::filesystem::path
 {
+    // Get the home directory from the environment variable "HOME"
+    const auto homeEnv = std::getenv("HOME");
+    if (homeEnv == nullptr)
+    {
+        std::cerr << "Error: HOME environment variable not set.\n";
+        return {};
+    }
+    const auto home = std::string(homeEnv);
+    if (home.empty())
+    {
+        std::cerr << "Error: HOME variable contains empty path.\n";
+        return {};
+    }
     return std::filesystem::path();
 }
